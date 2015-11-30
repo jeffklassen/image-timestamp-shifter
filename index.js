63710367a1ae2b22685c71a66cce974c3d6fb80a
Jeffrey Klassen <jeff.klassen@siginttech.com>
2015-11-30 14:59:06 +0100

    have comparison working

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var shiftTimestamp = require('./lib/shiftTimestamp');
-
+var piexif = require("piexifjs");
 var options = {
 	shiftAmount: -20820,
 	rootDir: '/Volumes/My Passport/tmp',
 	includedPhotosFilter: function(exif)
 	{
-		return true;
+		var should = true;
+		
+		if(exif["0th"][piexif.ImageIFD.Model]!="Canon EOS REBEL T3"){
+			should = false;
+		}
+		console.log(should);
+		return should;
 	}
 }
 
