f0c6e44c72c38098ced751441ec0c2b6287d75ad
Jeffrey Klassen <jeff.klassen@siginttech.com>
2015-12-01 07:20:22 +0100

    production ready

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 var shiftTimestamp = require('./lib/shiftTimestamp');
 var piexif = require("piexifjs");
+var utils = require('./lib/utils');
+
+var beforeDateTime = new Date(2014, 06, 10);
+console.log(beforeDateTime);
+
 var options = {
 	shiftAmount: -20820,
-	rootDir: '/Volumes/My Passport/tmp',
-	includedPhotosFilter: function(exif)
-	{
+	rootDir: '/sample/dir/here',
+	includedPhotosFilter: function (exif) {
 		var should = true;
+		var beforeDateTime = new Date(2014, 06, 10);
 		
-		if(exif["0th"][piexif.ImageIFD.Model]!="Canon EOS REBEL T3"){
+		if (exif["0th"][piexif.ImageIFD.Model] != "Canon EOS REBEL T3") {
 			should = false;
 		}
-		console.log(should);
+		if (beforeDateTime > utils.parseExifDate(exif["Exif"][piexif.ExifIFD.DateTimeOriginal])) {
+			should = false;
+		}
+
 		return should;
-	}
+	},
+	commitChanges: true
 }
 
 shiftTimestamp(options);
